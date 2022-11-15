7aea41d5d1f91601648a951e5c00a429599a8577
Graham Zemel <69569628+grahamzemel@users.noreply.github.com>
2022-11-15 10:30:41 -0500

    updated/fixed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,16 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import{ imagetools }from'vite-imagetools';
 
 export default defineConfig({
-  plugins: [imagetools({ force:true, optimizeAll:true, webp:true }), sveltekit()],
+  plugins: [imagetools({ force:true, optimizeAll:true, webp:true,
+    defaultDirectives: (url) => {
+      return{
+        quality: 75,
+        format: 'webp',
+        width: 672,
+        height: 448,
+      }
+    }
+   }), sveltekit()],
   server: {
     fs: {
       allow: ['./']
