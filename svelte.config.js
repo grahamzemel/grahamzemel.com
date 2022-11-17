f6182376765719a04f07e09ab4681c03b32359ee
Graham Zemel <69569628+grahamzemel@users.noreply.github.com>
2022-11-16 20:22:14 -0500

    revised build

--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,11 @@ import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
 import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-auto'
-import { cache } from 'sharp'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   //set cache control to one day
 
-  
+
   extensions: ['.svelte', ...mdsvexConfig.extensions],
   preprocess: [
     mdsvex(mdsvexConfig),
